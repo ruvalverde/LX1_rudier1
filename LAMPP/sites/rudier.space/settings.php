fa2e74ddd948d561b09b7b9e64af06ee679851d0
Rudier Valverde <ruvalverde@gmail.com>
2016-04-18 23:03:05 -0600

    Instalacion e Drupal en rudier.space

--- LAMPP/sites/rudier.space/settings.php
+++ LAMPP/sites/rudier.space/settings.php
@@ -212,7 +212,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'rudierdb',
+      'username' => 'rudieruser',
+      'password' => 'rvg',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -244,7 +258,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'oxmvk1iYa0QVrBR2mEhTXrDYXq392akejLBKGcLN0Sk';
 
 /**
  * Base URL (optional).
